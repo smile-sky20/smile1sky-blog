fe0a339a6240e6effa83957985a274698b482f8f
smile1sky <smile1sky@qq.com>
2024-10-16 09:33:11 +0800

    feat:css模块化学习

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,12 +1,20 @@
 'use client'
+import styles from './index.module.css' // 注意这里使用的是复数形式styles
 
-export default function Article(context: any) {
-  const { req, query, res, asPath, pathname } = context
-  const getInfo = () => {}
+export default function Article() {
+  const getInfo = () => {
+    console.log('按钮被点击了！')
+    // 这里可以添加更多的逻辑，比如调用API获取数据等
+  }
+
   return (
     <>
       <h1>这是文章详情</h1>
-      <button onClick={getInfo}>点击我</button>
+      <button type="button" className='btn-style' onClick={getInfo}>
+        {' '}
+        {/* 使用导入的样式类 */}
+        点击我
+      </button>
     </>
   )
 }
