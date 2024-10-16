1f3fc8e6812c2e8b192371763f3dcad8cca65f85
smile1sky <smile1sky@qq.com>
2024-10-16 09:36:25 +0800

    feat:新增权限页面，(auth)不出现在路由中

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+export default function Register() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Register</h1>
+    </div>
+  )
+}
