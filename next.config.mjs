1b91f6567af2afad336230180dc7a59172cb8c16
smile1sky <smile1sky@qq.com>
2024-10-18 18:00:21 +0800

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // 允许访问外部图片
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'camo.githubusercontent.com'
+      }
+    ]
+  }
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,40 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      backgroundColor: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        accents1: 'var(--accents-1)',
+        accents2: 'var(--accents-2)',
+        accents3: 'var(--accents-3)',
+        accents4: 'var(--accents-4)',
+        accents5: 'var(--accents-5)',
+        accents6: 'var(--accents-6)',
+        accents7: 'var(--accents-7)',
+        accents8: 'var(--accents-8)',
+      },
+      textColor: {
+        accents1: 'var(--accents-1)',
+        accents2: 'var(--accents-2)',
+        accents3: 'var(--accents-3)',
+        accents4: 'var(--accents-4)',
+        accents5: 'var(--accents-5)',
+        accents6: 'var(--accents-6)',
+        accents7: 'var(--accents-7)',
+        accents8: 'var(--accents-8)',
       },
     },
   },
   plugins: [],
-};
-export default config;
+  darkMode: 'class',
+}
+export default config
