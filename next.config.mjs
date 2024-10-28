e3f8dda0cec5f407579c53491624e945845781db
smile1sky <smile1sky@qq.com>
2024-10-28 17:02:35 +0800

    feat:新增config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Next.js 可以对链接进行静态类型检查，以防止在使用 next/link 时出现拼写错误和其他错误，从而提高在页面间导航时的类型安全性。
+  experimental: {
+    typedRoutes: true,
+  },
   images: {
     // 允许访问外部图片
     remotePatterns: [
