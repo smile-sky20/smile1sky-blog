ed3b020152def105ee714c3359c93025788dadda
smile1sky <smile1sky@qq.com>
2024-11-04 17:46:08 +0800

    feat:新增route文件

---
+++ src/app/api/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  console.log(123);
+  
+}
+
+export async function POST(request: NextRequest) {}
+
+export async function PUT(request: NextRequest) {}
+
+export async function DELETE(request: NextRequest) {}
+
+export async function PATCH(request: NextRequest) {}
+
+export async function OPTIONS(request: NextRequest) {}
+
+export async function HEAD(request: NextRequest) {}
