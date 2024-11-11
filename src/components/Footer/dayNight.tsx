14598f17ae25596f57cbd0877bc2ec3aa0d8c7f4
smile1sky <smile1sky@qq.com>
2024-11-11 09:48:52 +0800

    svg:切换主题更新

--- src/components/Footer/dayNight.tsx
+++ src/components/Footer/dayNight.tsx
@@ -1,3 +1,4 @@
+import { ImageComp } from '@/utils'
 import useThemeStore, { getTheme, theme } from '@/utils/useThemeStore'
 
 export function DayNight() {
@@ -6,54 +7,19 @@ export function DayNight() {
     useTheme.setTheme(theme)
   }
   return (
-    <div className="p-1 flex gap-2 border border-accents3 rounded-full">
+    <div className="relative p-1 flex gap-2 border border-accents3 rounded-full bg-transparent">
+      <div className={`absolute z-0 w-9 h-9 top-1 ${getTheme(useTheme, 'left-12 bg-purple-700', 'left-1 bg-accents3')} transition-all rounded-full`}></div>
       <button
-        className={`${getTheme(useTheme, '', 'bg-accents3')} p-2 rounded-full`}
+        className={`p-2 z-[1] rounded-full bg-transparent`}
         onClick={() => changeTheme('day')}
       >
-        <svg
-          stroke="currentColor"
-          fill="none"
-          strokeWidth="2"
-          viewBox="0 0 24 24"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          height="1em"
-          width="1em"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="12" cy="12" r="5"></circle>
-          <line x1="12" y1="1" x2="12" y2="3"></line>
-          <line x1="12" y1="21" x2="12" y2="23"></line>
-          <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-          <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-          <line x1="1" y1="12" x2="3" y2="12"></line>
-          <line x1="21" y1="12" x2="23" y2="12"></line>
-          <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-          <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-        </svg>
+        <ImageComp src="/sun.svg" alt="sun" width={20} height={20}></ImageComp>
       </button>
       <button
-        className={`${getTheme(useTheme, 'bg-accents3', '')} p-2 rounded-full`}
+        className={`z-[1] p-2 rounded-full`}
         onClick={() => changeTheme('dark')}
       >
-        <svg
-          stroke="currentColor"
-          fill="currentColor"
-          strokeWidth="0"
-          viewBox="0 0 512 512"
-          height="1em"
-          width="1em"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="32"
-            d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z"
-          ></path>
-        </svg>
+        <ImageComp src="/moon.svg" alt="sun" width={20} height={20}></ImageComp>
       </button>
     </div>
   )
