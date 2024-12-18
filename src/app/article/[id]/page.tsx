dcac3a395f50a2b27f23e6238f95367339e8af2b
smile1sky <smile1sky@qq.com>
2024-12-18 16:06:19 +0800

    refactor(app): 重构文章页面并优化数据获取逻辑
    - 移除多余的 fetchData 函数，使用 GET 函数替代
    - 简化 fetchUser 函数，提高代码可读性
    - 优化导入语句和类型定义
    - 调整布局和样式

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,41 +1,17 @@
 import { notFound } from 'next/navigation'
 import Image from 'next/image'
-export async function fetchData(
-  input: string | URL | globalThis.Request,
-  init?: RequestInit
-) {
-  const res = await fetch(input, init)
-  if (!res.ok) {
-    // 如果请求失败，抛出错误
-    throw new Error('Failed to fetch data')
-  }
-  return res.json()
-}
+import { GET } from '@/app/api/route'
 
 async function fetchUser(id: string) {
   // 如果路由是 /posts/1，那么 params.id 就是 1
-  const post = await fetchData(
-    `http://47.115.231.249/API/aiqing/api.php?type=json`,
-    {
-      method: 'get',
-      // 默认值"force-cache"，此请求应该被缓存，直到手动失效。
-
-      // 'no-store',此请求应该在每次请求时重新获取。
-      // cache: 'no-store',
-
-      // 设置资源的缓存生命周期 (以秒为单位)。
-      // revalidate: false(无限期缓存资源) | 0(防止资源被缓存。) | number((以秒为单位) 指定资源应该有最多 n 秒的缓存生命周期。)
-      // 1、如果同一路由中两个具有相同 URL 的 fetch 请求有不同的 revalidate 值，将使用较低的值。
-      // 2、为方便起见，如果设置了 revalidate 为数字，就不需要设置 cache 选项。
-      // 3、诸如 { revalidate: 3600, cache: 'no-store' } 这样的冲突选项将导致错误。
-      next: { revalidate: 3600 },
-    }
-  )
+  const post = await GET(`http://47.115.231.249/API/aiqing/api.php?type=json`, {
+    next: { revalidate: 3600 },
+  })
 
   // 获取拼音
-  const pinyin = await fetchData(
+  const pinyin = await GET(
     `http://47.115.231.249/API/pinyin/api.php?msg=${post.text}`,
-    { method: 'get', next: { revalidate: 3600 } }
+    { next: { revalidate: 3600 } }
   )
 
   // 将post.text字符串转换为数组，将拼音转为数组，然后将每个字对应每个拼音
@@ -54,14 +30,12 @@ async function fetchUser(id: string) {
   return result
 }
 
-export default async function Article(
-  props: {
-    params: Promise<{ id: string }>
-    searchParams: Promise<{ title: string }>
-  }
-) {
-  const searchParams = await props.searchParams;
-  const params = await props.params;
+export default async function Article(props: {
+  params: Promise<{ id: string }>
+  searchParams: Promise<{ title: string }>
+}) {
+  const searchParams = await props.searchParams
+  const params = await props.params
   const post = await fetchUser(params.id)
   const title = searchParams.title
 
@@ -76,7 +50,7 @@ export default async function Article(
           Thursday, October 24th 2024
         </div>
         <div className="title mt-6 mb-12 max-lg:mb-8 max-sm:mb-6 text-5xl  max-lg:text-4xl max-sm:text-2xl transition-all duration-300 font-semibold">
-          { title }
+          {title}
         </div>
 
         <div className="author text-accents4">Posted by</div>
