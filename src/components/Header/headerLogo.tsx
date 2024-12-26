3430a55d4a04d42da27b251998d211daf222dd4e
smile1sky <smile1sky@qq.com>
2024-12-26 16:46:04 +0800

    feat(components): 更新 Header 组件中的标题样式

--- src/components/Header/headerLogo.tsx
+++ src/components/Header/headerLogo.tsx
@@ -1,14 +1,14 @@
-import NextJsSvg from '@/ui/svg/nextJs'
+import { siYuan } from '@/ui/font'
 import SplitLineSvg from '@/ui/svg/splitLine'
 import VercelLogo from '@/ui/svg/vercelLogo'
 import Link from 'next/link'
 
 export function TitleLeft() {
   return (
-    <Link href={'/'} className="mr-6 flex justify-between items-center gap-x-1">
+    <Link href={'/'} className={`mr-6 flex justify-between items-center gap-x-1 ${siYuan}`}>
       <VercelLogo height={22}></VercelLogo>
       <SplitLineSvg height={32} width={32}></SplitLineSvg>
-      <NextJsSvg width={90} height={18}></NextJsSvg>
+      <div className='tracking-wider'>Smile1sky.</div>
     </Link>
   )
 }
