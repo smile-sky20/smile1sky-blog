739e44a5c1274a2b0eb4dfb267f307e7becbceb0
smile1sky <smile1sky@qq.com>
2024-12-27 16:06:55 +0800

    refactor(blog): 重构博客页面布局和样式

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,39 +2,19 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { inter } from '@/ui/font'
-import { lusitana } from '@/ui/font'
+import { siYuan } from '@/ui/font'
 
 import styles from './index.module.css'
 
-const linksArr = [
-  { name: 'Home', url: '/' },
-  { name: 'Blog', url: '/blog', svg: true },
-  { name: 'Article', url: '/article', svg: true },
-  { name: 'About', url: '/about' },
-]
-
 export default function Blog() {
   return (
-    <div className={`${lusitana.className} ${styles.back} text-2xl py-12 max-lg:px-6`}>
+    <div className={`${siYuan.className} ${styles.back} min-h-[80vh] text-2xl py-12 max-lg:px-6`}>
       <div
-        className={`card-warp-border p-1 flex justify-center items-center border border-accents3 rounded-3xl`}
+        className={`card-warp-border p-1 flex justify-center items-center border border-accents3`}
       >
         <div
-          className={`card-warp ${styles.cardBg} max-sm:px-4 max-md:px-6 max-lg:px-8 lg:px-12 py-6 bg-background border border-accents3 rounded-3xl shadow-2xl shadow-accents4`}
+          className={`card-warp ${styles.cardBg} max-sm:px-4 max-md:px-6 max-lg:px-8 lg:px-12 py-6 bg-background border border-accents3 shadow-2xl shadow-accents4`}
         >
-          <div
-            className={`card-tabs  m-auto px-3 py-2 w-96 flex justify-between border border-accents3 rounded-full`}
-          >
-            {linksArr.map((link, index) => (
-              <Link
-                key={link.name}
-                href={link.url}
-                className={`flex items-center text-sm text-accents7 hover:text-accents8 transition-colors`}
-              >
-                {link.name}
-              </Link>
-            ))}
-          </div>
           <div className="card-box mt-12 flex justify-center items-center transition-all duration-500">
             <div className="card-left mx-24 max-md:mx-20 flex flex-col max-md:items-center">
               <div className="text-base">My Name is</div>
@@ -59,7 +39,7 @@ export default function Blog() {
                 ))}
               </div>
 
-              <button className="py-1 px-[12px] mt-8 w-40 border  border-accents6 rounded-md bg-foreground text-accents1 text-sm font-semibold">
+              <button className="py-1 px-[12px] mt-8 w-40 border  border-accents6 bg-foreground text-accents1 text-sm font-semibold">
                 Learn
               </button>
             </div>
